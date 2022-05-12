fa8fc7ca62fc8615e5a0941283f05a543cbc5ae8
Bryam11 <xavierchuchuca18@gmail.com>
2022-05-11 19:34:15 -0500

    Create Structure_Project

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { AboutComponent } from "./about/about.component";
+import { InicioComponent } from "./inicio/inicio.component";
+import { PortafolioComponent } from "./portafolio/portafolio.component";
+import { QualificationComponent } from "./qualification/qualification.component";
+import { ServiceComponent } from "./service/service.component";
+import { SkillComponent } from "./skill/skill.component";
+import { TestimonialComponent } from "./testimonial/testimonial.component";
+import { ContactComponent } from './contact/contact.component';
+import { CommonModule } from "@angular/common";
+import { SwiperModule } from 'swiper/angular';
+
+
+@NgModule({
+    declarations: [
+        InicioComponent,
+        AboutComponent,
+        SkillComponent,
+        ServiceComponent,
+        QualificationComponent,
+        PortafolioComponent,
+        TestimonialComponent,
+        ContactComponent
+    ],
+    imports: [CommonModule, SwiperModule],
+    exports: [
+        InicioComponent,
+        AboutComponent,
+        SkillComponent,
+        ServiceComponent,
+        QualificationComponent,
+        PortafolioComponent,
+        TestimonialComponent,
+        ContactComponent
+    ],
+})
+
+export class PagesModule {}
